d7df22967b98af4c4ac63d600e2bcdfc4dc4e5ad
Justin Leniger <jtleniger@gmail.com>
2022-01-15 21:10:30 -0700

    auto build ulp c

---
+++ build_ulp.py
@@ -0,0 +1,11 @@
+import os
+
+print("Compiling ulp_c/ulp.c...")
+
+c_path = os.path.join("ulp_c", "ulp.c")
+
+os.system("ulpcc {}".format(c_path))
+
+s_path = os.path.join("ulp", "ulp.s")
+
+os.replace("ulp.s", "ulp\\ulp.s")
