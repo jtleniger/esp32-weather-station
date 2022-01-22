c222059fa9bca87ebf3070dc55e541665b7b3795
Justin Leniger <jtleniger@gmail.com>
2022-01-22 10:17:11 -0700

    cleanup cleanup

--- src/wifi.c
+++ src/wifi.c
@@ -32,10 +32,14 @@ static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_
     {
       system_event_sta_disconnected_t *event = (system_event_sta_disconnected_t *)event_data;
       wifi_err_reason_t reason = (wifi_err_reason_t)event->reason;
+
       ESP_LOGE(TAG, "reason: %d", reason);
+
       vTaskDelay(5000 / portTICK_PERIOD_MS);
       esp_wifi_connect();
+
       s_retry_num++;
+      
       ESP_LOGI(TAG, "retry to connect to the AP");
     }
     else
@@ -81,16 +85,20 @@ void connect_wifi()
   esp_event_handler_instance_t instance_any_id;
   esp_event_handler_instance_t instance_got_ip;
 
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                      ESP_EVENT_ANY_ID,
-                                                      &event_handler,
-                                                      NULL,
-                                                      &instance_any_id));
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                      IP_EVENT_STA_GOT_IP,
-                                                      &event_handler,
-                                                      NULL,
-                                                      &instance_got_ip));
+  ESP_ERROR_CHECK(
+    esp_event_handler_instance_register(
+      WIFI_EVENT,
+      ESP_EVENT_ANY_ID,
+      &event_handler,
+      NULL,
+      &instance_any_id));
+
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+    IP_EVENT,
+    IP_EVENT_STA_GOT_IP,
+    &event_handler,
+    NULL,
+    &instance_got_ip));
 
   wifi_country_t wifi_country = {
     .cc = "US ",
@@ -105,11 +113,7 @@ void connect_wifi()
       .sta = {
           .ssid = ESP_WIFI_SSID,
           .password = ESP_WIFI_PASS,
-          /* Setting a password implies station will connect to all security modes including WEP/WPA.
-             * However these modes are deprecated and not advisable to be used. Incase your Access point
-             * doesn't support WPA2, these mode can be enabled by commenting below line */
           .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-
           .pmf_cfg = {
               .capable = true,
               .required = false
@@ -125,23 +129,21 @@ void connect_wifi()
 
   /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
      * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-                                         pdFALSE,
-                                         pdFALSE,
-                                         portMAX_DELAY);
+  EventBits_t bits = xEventGroupWaitBits(
+    s_wifi_event_group,
+    WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+    pdFALSE,
+    pdFALSE,
+    portMAX_DELAY);
 
-  /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually happened. */
   if (bits & WIFI_CONNECTED_BIT)
   {
-    ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-             ESP_WIFI_SSID, ESP_WIFI_PASS);
+    ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
   }
   else if (bits & WIFI_FAIL_BIT)
   {
-    ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-             ESP_WIFI_SSID, ESP_WIFI_PASS);
+    ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
   }
   else
   {
@@ -151,5 +153,6 @@ void connect_wifi()
   /* The event will not be processed after unregister */
   ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
   ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+
   vEventGroupDelete(s_wifi_event_group);
 }
