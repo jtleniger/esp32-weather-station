d2e4cf5718b69584a3644cb9f44e61728fbb586d
Justin Leniger <jtleniger@gmail.com>
2022-01-30 14:54:24 -0700

    untested loop timing

--- ulp/ulp.s
+++ ulp/ulp.s
@@ -20,21 +20,27 @@ last_rain_value:
     .global reading
 reading:
 .long 0x0
-    .global sample
-sample:
-.long 0x0
     .global entry
 .text
 entry:
+reg_wr 3,31,31,1
 L.2:
+L.3:
+reg_rd 3,30,30
+move r0,r0 #if r0 == 0 goto L.2
+jump L.2, eq
+reg_rd 5,14,0
+move r1,last_sample_at
+st r0,r1,0
+L.5:
 move r2,reading
 ld r2,r2,0
 move r1,300
-sub r2,r2,r1 #{ if r2 < r1 goto L.6
+sub r2,r2,r1 #{ if r2 < r1 goto L.9
 add r2,r2,r1
-jump L.6, ov #}
-jump L.18
-L.6:
+jump L.9, ov #}
+jump L.24
+L.9:
 reg_rd 265,30,30
 move r1,wind_value
 st r0,r1,0
@@ -44,29 +50,29 @@ st r0,r1,0
 move r2,last_rain_value
 ld r2,r2,0
 move r1,0xffff
-sub r2,r2,r1 #{ if r2==r1 goto L.8 
+sub r2,r2,r1 #{ if r2==r1 goto L.11 
 jump 1f, eq
 add r2,r2,r1
 jump 2f
 1:
 add r2,r2,r1
-jump L.8
+jump L.11
 2:           #}
 move r1,rain_value
 ld r1,r1,0
-sub r1,r1,r2 #{ if r1==r2 goto L.8 
+sub r1,r1,r2 #{ if r1==r2 goto L.11 
 jump 1f, eq
 add r1,r1,r2
 jump 2f
 1:
 add r1,r1,r2
-jump L.8
+jump L.11
 2:           #}
 move r2,rain_edges
 ld r1,r2,0
 add r1,r1,1
 st r1,r2,0
-L.8:
+L.11:
 move r2,last_rain_value
 move r1,rain_value
 ld r1,r1,0
@@ -74,44 +80,54 @@ st r1,r2,0
 move r2,last_wind_value
 ld r2,r2,0
 move r1,0xffff
-sub r2,r2,r1 #{ if r2==r1 goto L.10 
+sub r2,r2,r1 #{ if r2==r1 goto L.13 
 jump 1f, eq
 add r2,r2,r1
 jump 2f
 1:
 add r2,r2,r1
-jump L.10
+jump L.13
 2:           #}
 move r1,wind_value
 ld r1,r1,0
-sub r1,r1,r2 #{ if r1==r2 goto L.10 
+sub r1,r1,r2 #{ if r1==r2 goto L.13 
 jump 1f, eq
 add r1,r1,r2
 jump 2f
 1:
 add r1,r1,r2
-jump L.10
+jump L.13
 2:           #}
 move r2,wind_edges
 ld r1,r2,0
 add r1,r1,1
 st r1,r2,0
-L.10:
+L.13:
 move r2,last_wind_value
 move r1,wind_value
 ld r1,r1,0
 st r1,r2,0
-move r2,sample
-ld r1,r2,0
-add r1,r1,1
-st r1,r2,0
+reg_wr 3,31,31,1
+L.15:
+L.16:
+reg_rd 3,30,30
+move r0,r0 #if r0 == 0 goto L.15
+jump L.15, eq
+reg_rd 5,14,0
+move r1,this_sample_at
+st r0,r1,0
+move r2,this_sample_at
 ld r2,r2,0
-move r1,31496
-sub r2,r2,r1 #{ if r2 < r1 goto L.2
+move r1,last_sample_at
+ld r1,r1,0
+sub r2,r2,r1
+move r1,122
+sub r2,r2,r1 #{ if r2 < r1 goto L.5
 add r2,r2,r1
-jump L.2, ov #}
-move r2,sample
-move r1,0
+jump L.5, ov #}
+move r2,last_sample_at
+move r1,this_sample_at
+ld r1,r1,0
 st r1,r2,0
 move r2,reading
 ld r2,r2,0
@@ -128,20 +144,26 @@ move r2,reading
 ld r1,r2,0
 add r1,r1,1
 st r1,r2,0
-jump L.2
-L.17:
-L.18:
+jump L.5
+L.23:
+L.24:
 reg_rd 48,19,19
 and r2,r0,1
-move r2,r2 #if r2 == 0 goto L.17
-jump L.17, eq
+move r2,r2 #if r2 == 0 goto L.23
+jump L.23, eq
 wake 
 reg_wr 6,24,24,0
 halt 
-L.15:
+L.21:
 L.1:
 
 .bss
+    .global this_sample_at
+this_sample_at:
+    .space 4
+    .global last_sample_at
+last_sample_at:
+    .space 4
     .global wind_ticks
 wind_ticks:
     .space 1200
--- ulp_c/ulp.c
+++ ulp_c/ulp.c
@@ -3,7 +3,7 @@
 #define WIND_PIN 16
 #define RAIN_PIN 6
 
-#define SAMPLES_PER_READING 31496
+#define RTC_CNTL_TIME1_1_SEC 0x7A
 #define MAX_READING 300
 
 unsigned int rain_edges = 0;
@@ -17,46 +17,54 @@ unsigned int rain_value = 0;
 unsigned int last_rain_value = 0xFFFF;
 
 unsigned int reading = 0;
-unsigned int sample = 0;
+unsigned int last_sample_at;
+unsigned int this_sample_at;
 
 void entry()
 {
-    for (;;)
+  // Initialize to now
+  WRITE_RTC_FIELD(RTC_CNTL_TIME_UPDATE_REG, RTC_CNTL_TIME_UPDATE, 1);
+  while (!READ_RTC_FIELD(RTC_CNTL_TIME_UPDATE_REG, RTC_CNTL_TIME_VALID));
+  last_sample_at = READ_RTC_REG(RTC_CNTL_TIME1_REG, 0, 15);
+
+  for (;;)
+  {
+    if (reading >= MAX_READING)
     {
-        if (reading >= MAX_READING)
-        {
-            break;
-        }
-
-        wind_value = READ_RTC_REG(RTC_GPIO_IN_REG, RTC_GPIO_IN_NEXT_S + WIND_PIN, 1);
-        rain_value = READ_RTC_REG(RTC_GPIO_IN_REG, RTC_GPIO_IN_NEXT_S + RAIN_PIN, 1);
-
-        if (last_rain_value != 0xFFFF && rain_value != last_rain_value)
-        {
-            rain_edges++;
-        }
-
-        last_rain_value = rain_value;
-
-        if (last_wind_value != 0xFFFF && wind_value != last_wind_value)
-        {
-            wind_edges++;
-        }
-
-        last_wind_value = wind_value;
-
-        sample++;
-
-        if (sample >= SAMPLES_PER_READING)
-        {
-            sample = 0;
-
-            wind_ticks[reading] = wind_edges >> 1;
-            wind_edges = 0;
-
-            reading++;
-        }    
+      break;
     }
-    
-    wake_when_ready();
+
+    wind_value = READ_RTC_REG(RTC_GPIO_IN_REG, RTC_GPIO_IN_NEXT_S + WIND_PIN, 1);
+    rain_value = READ_RTC_REG(RTC_GPIO_IN_REG, RTC_GPIO_IN_NEXT_S + RAIN_PIN, 1);
+
+    if (last_rain_value != 0xFFFF && rain_value != last_rain_value)
+    {
+      rain_edges++;
+    }
+
+    last_rain_value = rain_value;
+
+    if (last_wind_value != 0xFFFF && wind_value != last_wind_value)
+    {
+      wind_edges++;
+    }
+
+    last_wind_value = wind_value;
+
+    WRITE_RTC_FIELD(RTC_CNTL_TIME_UPDATE_REG, RTC_CNTL_TIME_UPDATE, 1);
+    while (!READ_RTC_FIELD(RTC_CNTL_TIME_UPDATE_REG, RTC_CNTL_TIME_VALID));
+    this_sample_at = READ_RTC_REG(RTC_CNTL_TIME1_REG, 0, 15);
+
+    if ((this_sample_at - last_sample_at) >= RTC_CNTL_TIME1_1_SEC)
+    {
+      last_sample_at = this_sample_at;
+
+      wind_ticks[reading] = wind_edges >> 1;
+      wind_edges = 0;
+
+      reading++;
+    }
+  }
+
+  wake_when_ready();
 }
